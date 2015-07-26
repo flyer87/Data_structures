50850f10334dfeed1496a84e8cb84dd21bc28c70
Omer Yumer <y.yumer@gmail.com>
2015-07-26 20:48:29 +0200

    4. Trees-and-Tree-Like-Structures Problem 01. Solved

---
+++ 4. Trees-and-Tree-Like-Structures/01_PlayWithTrees/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_PlayWithTrees;
+
+namespace _01_PlayWithTrees
+{
+    class Program
+    {
+        static void Main()
+        {
+            Tree<int> corner = new Tree<int>(0);
+            int nodeCnts = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nodeCnts - 1; i++)
+            {
+                string[] edge = Console.ReadLine().Split(' ');
+                int parentValue = int.Parse(edge[0]);
+                Tree<int> parentNode = Tree<int>.GetTreeNodeByValue(parentValue);
+                int childValue = int.Parse(edge[1]);
+                Tree<int> childNode = Tree<int>.GetTreeNodeByValue(childValue);
+                parentNode.Children.Add(childNode);
+                childNode.Parent = parentNode;
+                if (i == 0)
+                {
+                    corner = parentNode;
+                }
+            }
+
+            int pathSum = int.Parse(Console.ReadLine());
+            int subTreeSum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            corner.Print();
+            
+        }
+
+        
+    }
+}
---
+++ 4. Trees-and-Tree-Like-Structures/01_PlayWithTrees/Tree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_PlayWithTrees
+{
+    public class Tree<T>
+    {
+        public T Value { get; set; }
+        public Tree<T> Parent { get; set; }
+        public IList<Tree<T>> Children { get; set; }
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+            foreach (var child in children)
+            {
+                this.Children.Add(child);
+                child.Parent = this;
+            }
+        }
+
+        public void Print(int indent = 0)
+        {
+            Console.Write(new String(' ', 2 * indent));
+            Console.WriteLine(this.Value);
+            foreach (var child in this.Children)
+            {
+                child.Print(indent + 1);
+            }
+        }
+
+        static Dictionary<T, Tree<T>> nodeByValue = new Dictionary<T, Tree<T>>();
+
+        public static Tree<T> GetTreeNodeByValue(T value){
+            if (!nodeByValue.ContainsKey(value))
+            {
+                nodeByValue[value] = new Tree<T>(value);
+            }
+
+            return nodeByValue[value];
+        }
+
+        public static Tree<T> FindRootNode()
+        {
+            var rootNode = nodeByValue.Values.FirstOrDefault(node => node.Parent == null);
+
+            return rootNode;
+        }
+
+        public static IList<Tree<T>> FindMiddlenodes()
+        {
+            var middleNodes = nodeByValue.Values.Where(node => node.Children.Count > 0 && node.Parent != null).ToList();
+
+            return middleNodes;
+        }
+    }
+}
