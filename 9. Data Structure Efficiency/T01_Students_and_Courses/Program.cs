328408e2cab0959eec9e4a58262a3d1ba349f683
Omer Yumer <y.yumer@gmail.com>
2015-09-04 17:43:28 +0200

    9. Data Structure Efficiency - poblem 1. solved

---
+++ 9. Data Structure Efficiency/T01_Students_and_Courses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Wintellect.PowerCollections;
+using System.Text;
+
+namespace T01_Students_and_Courses
+{
+    class Program
+    {
+        static OrderedDictionary<string, SortedSet<Student>> studentsByCourse =
+                  new OrderedDictionary<string, SortedSet<Student>>();
+
+        static void AddStudents()
+        {            
+            if (!File.Exists("StudentData.txt"))
+            {
+                throw new FileNotFoundException("StudentData.txt");
+            }
+
+            foreach (string line in File.ReadLines("StudentData.txt"))
+            {
+                string[] tokens = line.Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries);
+                string firstName = tokens[0].Trim();
+                string lastName = tokens[1].Trim();
+                string course = tokens[2].Trim();
+
+                var student = new Student { FirstName = firstName, LastName = lastName };
+                studentsByCourse.AppendValueToKey(course, student);
+            }
+        }
+
+        static void PrintStudents()
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (KeyValuePair<string, SortedSet<Student>> students in studentsByCourse)
+            {
+                StringBuilder studentsAsString = new StringBuilder();
+                foreach (var student in students.Value)
+                {
+                    studentsAsString.Append(student.FirstName + " " + student.LastName + ", ");
+                }
+                string course = students.Key + ": ";
+                result.AppendLine(course + studentsAsString);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            AddStudents();
+            PrintStudents();
+        }
+    }
+}
---
+++ 9. Data Structure Efficiency/T01_Students_and_Courses/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T01_Students_and_Courses
+{
+    public class Student : IComparable<Student>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            return this.LastName.CompareTo(other.LastName);
+        }
+    }
+}
