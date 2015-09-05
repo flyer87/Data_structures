f8b810097f4ad86536687c7a9783958148fe9681
Omer Yumer <y.yumer@gmail.com>
2015-09-05 12:35:28 +0200

    9. Data Structure Efficiency - poblem 2. solved

---
+++ 9. Data Structure Efficiency/T02_BiDictionary/BiDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T02_BiDictionary
+{
+    public class BiDictionary<K1, K2, T>
+    {
+        private Dictionary<K1, List<T>> valuesByFirstKey = new Dictionary<K1, List<T>>();
+        private Dictionary<K2, List<T>> valuesBySecondKey = new Dictionary<K2, List<T>>();
+        private Dictionary<Tuple<K1, K2>, List<T>> valuesByBothKeys = 
+            new Dictionary<Tuple<K1, K2>, List<T>>();
+
+        public void Add(K1 key1, K2 key2, T value)
+        {
+            valuesByFirstKey.AppendValueToKey(key1, value);
+            valuesBySecondKey.AppendValueToKey(key2, value);
+            valuesByBothKeys.AppendValueToKey(new Tuple<K1, K2>(key1, key2), value);
+        }
+
+        public IEnumerable<T> Find(K1 key1, K2 key2)
+        {
+            return this.valuesByBothKeys.GetValuesForKeys(key1, key2);            
+        }
+        public IEnumerable<T> FindByKey1(K1 key1)
+        {
+            return valuesByFirstKey.GetValuesForKey(key1);                  
+        }
+        public IEnumerable<T> FindByKey2(K2 key2)
+        {
+            return valuesBySecondKey.GetValuesForKey(key2);                  
+        }
+        public bool Remove(K1 key1, K2 key2)
+        {
+            var dictances = this.Find(key1, key2);
+            if (dictances == null)
+            {
+                return false;
+            }
+            
+            foreach (var distance in dictances)
+            {
+                valuesByFirstKey[key1].Remove(distance);
+                valuesBySecondKey[key2].Remove(distance);
+            }
+            valuesByBothKeys[new Tuple<K1, K2>(key1, key2)].Clear();
+            return true;
+            
+        }
+    }
+}
---
+++ 9. Data Structure Efficiency/T02_BiDictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T02_BiDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var distances = new BiDictionary<string, string, int>();
+            distances.Add("Sofia", "Varna", 443);
+            distances.Add("Sofia", "Varna", 468);
+            distances.Add("Sofia", "Varna", 490);
+            distances.Add("Sofia", "Plovdiv", 145);
+            distances.Add("Sofia", "Bourgas", 383);
+            distances.Add("Plovdiv", "Bourgas", 253);
+            distances.Add("Plovdiv", "Bourgas", 292);
+            var distancesFromSofia = distances.FindByKey1("Sofia"); // [443, 468, 490, 145, 383]
+            Print(distancesFromSofia);
+            var distancesToBourgas = distances.FindByKey2("Bourgas"); // [383, 253, 292]
+            Print(distancesToBourgas);
+            var distancesPlovdivBourgas = distances.Find("Plovdiv", "Bourgas"); // [253, 292]
+            Print(distancesPlovdivBourgas);
+            var distancesRousseVarna = distances.Find("Rousse", "Varna"); // []
+            Print(distancesRousseVarna);
+            var distancesSofiaVarna = distances.Find("Sofia", "Varna"); // [443, 468, 490]
+            Print(distancesSofiaVarna);            
+            distances.Remove("Sofia", "Varna"); // true
+            var distancesFromSofiaAgain = distances.FindByKey1("Sofia"); // [145, 383]
+            Print(distancesFromSofiaAgain);
+            var distancesToVarna = distances.FindByKey2("Varna"); // []
+            Print(distancesToVarna);
+            var distancesSofiaVarnaAgain = distances.Find("Sofia", "Varna"); // []
+            Print(distancesSofiaVarnaAgain);
+        }
+
+        static void Print(IEnumerable<int> list)
+        {
+            string distancesStr = "";
+            foreach (var item in list)
+            {
+                distancesStr = distancesStr + item + ", ";
+            }
+
+            Console.WriteLine(distancesStr);
+        }
+    }
+}
